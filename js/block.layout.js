ab31a7066b0124cb1f41163920896695b5c1334b
Jin sung Lee <jlee@cogentpa.co.kr>
2019-01-17 19:16:52 +0900

    확대 / 축소

--- js/block.layout.js
+++ js/block.layout.js
@@ -1,6 +1,5 @@
 var rules = Cal.init();
 
-
 $("#propModal").on("show.bs.modal", function (e) {
     $("#propPageNm").val(Layout.getPageInfo("name"));
     $("#bgWidth").val($("#svg-container").width());
@@ -164,6 +163,7 @@ function initNodeProp(){
 }
 
 function nodeSelect(obj){
+    console.log(obj);
     if(obj == null){
         initNodeProp();
         return;
@@ -187,48 +187,6 @@ function nodeSelect(obj){
     $("#prop_blockColor").spectrum("set", nodeObj.color||"#000000");
     $('#rightTab_block').tab('show');
 }
-
-$("#firstLoading").hide();
-
-function window_resize(){
-    var winHeight = window.innerHeight||document.body.clientHeight;
-    if(parseInt(winHeight) < 1){
-        winHeight = document.body.clientHeight;
-    }
-    $("#container").height(winHeight-76);
-}
-window_resize();
-$(window).bind("resize", function(){
-    window_resize();
-});
-
-function init(){
-    Cal.destory();
-    $("#prop_blockColor").spectrum({
-        color: "#000000",
-        showPalette: true,
-        palette: [
-            ["#000","#444","#666","#999","#ccc","#eee","#f3f3f3","#fff"],
-            ["#f00","#f90","#ff0","#0f0","#0ff","#00f","#90f","#f0f"],
-            ["#f4cccc","#fce5cd","#fff2cc","#d9ead3","#d0e0e3","#cfe2f3","#d9d2e9","#ead1dc"],
-            ["#ea9999","#f9cb9c","#ffe599","#b6d7a8","#a2c4c9","#9fc5e8","#b4a7d6","#d5a6bd"],
-            ["#e06666","#f6b26b","#ffd966","#93c47d","#76a5af","#6fa8dc","#8e7cc3","#c27ba0"],
-            ["#c00","#e69138","#f1c232","#6aa84f","#45818e","#3d85c6","#674ea7","#a64d79"],
-            ["#900","#b45f06","#bf9000","#38761d","#134f5c","#0b5394","#351c75","#741b47"],
-            ["#600","#783f04","#7f6000","#274e13","#0c343d","#073763","#20124d","#4c1130"]
-        ]
-    });
-    $("#prop_blockColor").bind("change", function(){
-        var selectNode = Layout.getBlock();
-        if(selectNode){
-            selectNode.color = this.value;
-            Diagrams.updateNode();
-        }
-    });
-}
-
-init();
-
 function calClick(id){
     var d = CalData.getCalById(id);
     CalData.setSelect(d);
@@ -371,3 +329,65 @@ $("#prop_calOpen").bind("click", function(){
     Cal.open(window.cals);
 });
 
+$("#firstLoading").hide();
+
+function window_resize(){
+    var winHeight = window.innerHeight||document.body.clientHeight;
+    if(parseInt(winHeight) < 1){
+        winHeight = document.body.clientHeight;
+    }
+    $("#container").height(winHeight-86);
+    //$("#svg-container").height(winHeight-86);
+    $("#svg-container").width(2000);
+    $("#svg-container").height(2000);
+}
+window_resize();
+$(window).bind("resize", function(){
+    window_resize();
+});
+
+function layout_init(){
+    Cal.destory();
+    $("#prop_blockColor").spectrum({
+        color: "#000000",
+        showPalette: true,
+        palette: [
+            ["#000","#444","#666","#999","#ccc","#eee","#f3f3f3","#fff"],
+            ["#f00","#f90","#ff0","#0f0","#0ff","#00f","#90f","#f0f"],
+            ["#f4cccc","#fce5cd","#fff2cc","#d9ead3","#d0e0e3","#cfe2f3","#d9d2e9","#ead1dc"],
+            ["#ea9999","#f9cb9c","#ffe599","#b6d7a8","#a2c4c9","#9fc5e8","#b4a7d6","#d5a6bd"],
+            ["#e06666","#f6b26b","#ffd966","#93c47d","#76a5af","#6fa8dc","#8e7cc3","#c27ba0"],
+            ["#c00","#e69138","#f1c232","#6aa84f","#45818e","#3d85c6","#674ea7","#a64d79"],
+            ["#900","#b45f06","#bf9000","#38761d","#134f5c","#0b5394","#351c75","#741b47"],
+            ["#600","#783f04","#7f6000","#274e13","#0c343d","#073763","#20124d","#4c1130"]
+        ]
+    });
+    $("#prop_blockColor").bind("change", function(){
+        var selectNode = Layout.getBlock();
+        if(selectNode){
+            selectNode.color = this.value;
+            Diagrams.updateNode();
+        }
+    });
+    $("#slider").slider({
+        min : 0,
+        max : 20,
+        value : 10,
+        slide: function(event, ui) {
+            var panZoomTiger = svgPanZoom('#diagram');
+            var zoom = ui.value/10;
+            panZoomTiger.zoom(zoom);
+        }
+    });
+    $("#zoomReset").bind("click", function(){
+        var panZoomTiger = svgPanZoom('#diagram');
+        $("#slider").slider( "value", 10);
+        panZoomTiger.zoom(1);
+    });
+    window.onload = function() {
+        var svgActive = false, svgHovered = false;
+        window.panZoom = svgPanZoom('#diagram', {zoomEnabled: true, controlIconsEnabled: false,mouseWheelZoomEnabled: true});
+    };
+}
+
+layout_init();
