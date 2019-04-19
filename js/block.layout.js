40ff0cdf2078afed169e3ac88db81f715f510646
immb <work.immb@gmail.com>
2019-04-19 17:35:53 +0900

    zoom error

--- js/block.layout.js
+++ js/block.layout.js
@@ -568,6 +568,13 @@ function layout_init(){
     });
     var Scrollbar = window.Scrollbar;
     Scrollbar.init(document.querySelector('#container .right'), {});
+    /*
+    var viewportGroupElement = document.getElementById('diagram').querySelector('.viewport');
+    svgPanZoom('#diagram', {
+        viewportSelector: viewportGroupElement
+      });
+    */  
+    window.panZoom = svgPanZoom('#diagram', {zoomEnabled: true, controlIconsEnabled: false,mouseWheelZoomEnabled: false, dblClickZoomEnabled:false, center:true});
 }
 
 
